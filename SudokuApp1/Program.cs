c58e261a4ea966d7b2d37d17cdd8e998241d1d9b
sxmscelik <s.celik@exeliatech.com>
2020-03-07 11:38:21 +0200

    sudokuapp1

---
+++ SudokuApp1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SudokuApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[][] goodSudoku1 = {
+                new int[] {7,8,4,  1,5,9,  3,2,6},
+                new int[] {5,3,9,  6,7,2,  8,4,1},
+                new int[] {6,1,2,  4,3,8,  7,5,9},
+
+                new int[] {9,2,8,  7,1,5,  4,6,3},
+                new int[] {3,5,7,  8,4,6,  1,9,2},
+                new int[] {4,6,1,  9,2,3,  5,8,7},
+
+                new int[] {8,7,6,  3,9,4,  2,1,5},
+                new int[] {2,4,3,  5,6,1,  9,7,8},
+                new int[] {1,9,5,  2,8,7,  6,3,4}
+            };
+
+
+            int[][] goodSudoku2 = {
+                new int[] {1,3,2,4},
+                new int[] {4,2,1,3},
+                new int[] {3,1,4,2},
+                new int[] {2,4,3,1}
+            };
+
+            //--------------------------------------------------------------------------------------------------------------
+
+            int[][] badSudoku1 =  {
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9}
+            };
+
+            int[][] badSudoku2 = {
+                new int[] {1,2,3,4,5},
+                new int[] {1,2,3,4},
+                new int[] {1,2,3,4},
+                new int[] {1}
+            };
+
+            int[][] badSudoku3 = {
+                new int[] {7,8,4,  0,5,1,  3,2,6},
+                new int[] {5,3,1,  6,7,2,  8,4,0},
+                new int[] {6,0,2,  4,3,8,  7,5,1},
+
+                new int[] {1,2,8,  7,0,5,  4,6,3},
+                new int[] {3,5,7,  8,4,6,  0,1,2},
+                new int[] {4,6,0,  1,2,3,  5,8,7},
+
+                new int[] {8,7,6,  3,1,4,  2,0,5},
+                new int[] {2,4,3,  5,6,0,  1,7,8},
+                new int[] {0,1,5,  2,8,7,  6,3,4}
+            };
+            int[][] badSudoku4 =  {
+                new int[] {1,1,1, 1,1,1, 1,1,1},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9}
+            };
+
+            int[][] badSudoku5 = {
+                new int[] {1,4, 2},
+                new int[] {3,2, 4},
+
+                new int[] {4,1, 3}
+            };
+
+
+            ValidateSudoku validate = new ValidateSudoku();
+
+
+            Console.WriteLine();
+
+            var rowColEqual = validate.IsSudokuNumGreaterThanZeroLessThanNine(badSudoku5);
+            Console.WriteLine($"Rows and columns of Sudoku has equal length: {rowColEqual}");
+
+            /*
+            var rowColEqual = validate.IsRowsColumnsEqual(badSudoku3);
+            Console.WriteLine($"Rows and columns of Sudoku has equal length: {rowColEqual}");
+
+
+            var rowValid = validate.IsRowsDuplicate(badSudoku3);
+            Console.WriteLine($"Rows are not duplicate: {rowValid}");
+
+
+            var colValid = validate.IsColumnsDuplicate(badSudoku3);
+            Console.WriteLine($"Columns are not duplicate: {colValid} \n");
+           
+
+
+            var final = validate.FinalValidate(badSudoku3);
+            Console.WriteLine($"It's a good sudoku!: { final}");
+            */
+        }
+
+    }
+
+}
---
+++ SudokuApp1/ValidateSudoku.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SudokuApp1
+{
+    public class ValidateSudoku
+    {
+
+        public bool IsSudokuNumGreaterThanZeroLessThanNine(int[][] sudoku)
+        {
+            for (int i = 0; i < sudoku.Length; i++)
+            {
+                for (int j = 0; j < sudoku[i].Length; j++)
+                {
+                    if (sudoku[i][j] <= 0)
+                    {
+                        return false;
+                    }
+                    else if (sudoku[i][j] > 9)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+
+
+        /*
+        
+        public bool IsRowsAreDuplicate(int[][] sudoku)              //Checks for duplicate rows
+        {
+           
+            for (int i = 0; i < sudoku.Length; i++)
+            {
+                for (int j = 0; j < sudoku.Length-1; j++)
+                {
+                    if (sudoku[i][j] == sudoku[i][j+1])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        
+          
+        public bool IsColumnsDuplicate(int[][] sudoku)           //Checks for duplicate columns
+        {
+            if (IsRowsColumnsEqual(sudoku) == false || sudoku.Length == 0)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < (sudoku.Length-1); i++)
+            {
+                for (int j = 0; j < sudoku.Length; j++)
+                {
+                    if (sudoku[i][j] == sudoku[i+1][j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        
+
+        public bool IsRowsColumnsEqual(int[][] sudoku)               // NxN
+        {
+            int rows = sudoku.Length;
+            int cols;
+
+            for (int i = 0; i < sudoku.Length; i++)
+            {
+                cols = sudoku[i].Length;
+            }
+
+           
+
+            for (int i = 0; i < sudoku.Length; i++)
+            {
+                cols = sudoku[i].Length;
+
+                if (rows != cols)
+                {
+                    return false;
+                }
+               
+            }
+            return true;
+        }
+
+
+        public bool FinalValidate(int[][] sudoku)
+        {
+            if (IsColumnsDuplicate(sudoku) == false || IsRowsDuplicate(sudoku) == false || IsRowsColumnsEqual(sudoku) == false)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        */
+
+    }
+
+}
